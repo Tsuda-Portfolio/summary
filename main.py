7e9cef0b149fcf59b08f888546a2a416af28a5df
Tsuda-Portfolio <150001557+Tsuda-Portfolio@users.noreply.github.com>
2023-11-09 20:03:56 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
-from summary import ja_summary, en_to_ja_summary, ja_to_en_summary, yahoonews_summary
+from summary import ja_summary, en_summary, en_to_ja_summary, ja_to_en_summary, yahoonews_summary, home_news, search_news
 
 
 app = Flask(__name__)
 
 
+# HomeのHTML
 @app.route("/")
 def index():
-    return render_template("index.html")
+    home_News = home_news()
+    return render_template("index.html", home_News=home_News)
 
 
+# 日本語要約のHTML
 @app.route("/ja_summary", methods=["GET", "POST"])
 def japanese():
     # POSTのとき
@@ -22,6 +25,20 @@ def japanese():
     return render_template("ja_summary.html")
 
 
+# 英語要約のHTML
+@app.route("/en_summary", methods=["GET", "POST"])
+def english():
+    # POSTのとき
+    if request.method == "POST":
+        TEXT = request.form["keyword"]
+        LIMIT = request.form["limit"]
+        english_summary = en_summary(TEXT, LIMIT)
+        return render_template("en_summary.html", english_summary=english_summary)
+    # GETのとき
+    return render_template("en_summary.html")
+
+
+# 英語から日本語要約のHTML
 @app.route("/en_to_ja_summary", methods=["GET", "POST"])
 def en_to_ja():
     # POSTのとき
@@ -36,6 +53,7 @@ def en_to_ja():
     return render_template("en_to_ja_summary.html")
 
 
+# 日本語から英語要約のHTML
 @app.route("/ja_to_en_summary", methods=["GET", "POST"])
 def ja_to_en():
     # POSTのとき
@@ -51,6 +69,7 @@ def ja_to_en():
     return render_template("ja_to_en_summary.html")
 
 
+# Yahoo!News要約のHTML
 @app.route("/yahoonews", methods=["GET", "POST"])
 def yahoo():
     # POSTのとき
--- summary.py
+++ summary.py
@@ -1,4 +1,5 @@
 import os
+import html
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -12,7 +13,7 @@ from pysummarization.similarityfilter.tfidf_cosine import TfIdfCosine
 from pysummarization.tokenizabledoc.mecab_tokenizer import MeCabTokenizer
 
 
-# ----------日本語の文章の要約---------- #
+# ----------日本語の文章の要約(要約の度合いを決めれる)---------- #
 def ja_summary(TEXT, LIMIT):
     LIMIT = float(LIMIT)
 
@@ -76,7 +77,7 @@ def ja_summary(TEXT, LIMIT):
     return ja_summary
 
 
-# ----------英語の文章の要約---------- #
+# ----------英語の文章の要約(要約の度合いを決めれる)---------- #
 def en_summary(TEXT, LIMIT):
 
     # LIMITをfloat型に修正する
@@ -145,7 +146,7 @@ def en_summary(TEXT, LIMIT):
     return en_summary
 
 
-# ----------英語→日本語の文章要約---------- #
+# ----------英語→日本語の文章要約(要約の度合いを決めれる)---------- #
 def en_to_ja_summary(TEXT, LIMIT):
     LIMIT = float(LIMIT)
     # 空のリストを用意
@@ -238,7 +239,7 @@ def en_to_ja_summary(TEXT, LIMIT):
     return summary
 
 
-# ----------日本語→英語の文章要約---------- #
+# ----------日本語→英語の文章要約(要約の度合いを決めれる)---------- #
 def ja_to_en_summary(TEXT, LIMIT):
 
     # LIMITをfloat型に修正する
@@ -333,7 +334,7 @@ def ja_to_en_summary(TEXT, LIMIT):
     return summary
 
 
-# ----------ヤフーニュースの要約---------- #
+# ----------ヤフーニュースの要約(API使用なし)---------- #
 def yahoonews_summary(COUNT, LIMIT):
 
     # LIMITをfloat型に修正する
@@ -453,4 +454,143 @@ def yahoonews_summary(COUNT, LIMIT):
         if i == COUNT or i == 8:
             return yahoo_news_list
 
-print(en_to_ja_summary('hello', 0.3))
+
+# ----------ニュースサイト要約(ホームページ用)----------#
+def home_news():
+
+    # ニュースを格納する空のリスト
+    news_list = []
+
+    # ニュースapiのurl
+    url = 'https://newsapi.org/v2/top-headlines'
+
+    # 環境変数からAPIKEYを取得
+    load_dotenv()
+
+    NEWS_API_KEY = os.environ["NEWS_API_KEY"]
+
+    # ヘッダーの設置
+    headers = {'X-Api-Key': NEWS_API_KEY}
+
+    params = {   
+        # 国名
+        'country': 'jp',
+        # 最大何件取得するか
+        'pageSize': 5,
+        # 人気のニュースをピックアップ
+        'sortBy': 'popularity'
+    }
+
+    # リクエストを送信してレスポンスを取得
+    response = requests.get(url, headers=headers, params=params)
+
+    # 記事が取得できたか否か
+    if response.status_code == 200:
+        # JSON形式のデータを取得
+        news_data = response.json()
+        # 記事を取得
+        articles = news_data['articles']
+
+        # 記事を表示
+        for article in articles:
+            # タイトル
+            title = html.escape(article['title'])
+            # URL
+            url = html.escape(article['url'])
+            # 画像のURL
+            image_url = html.escape(article['urlToImage'])
+
+            if article['description'] is not None:
+                # 記事の内容
+                content = html.escape(article['description'])
+            else:
+                content = '記事が取得できませんでした。'
+
+            # リストに追加
+            news_list.append({
+                'title': title,
+                'url': url,
+                'image_url': image_url,
+                'content': content
+            })
+
+    else:
+        news_list.append({
+            'title': 'タイトルの取得に失敗しました。',
+            'url': 'URLの取得に失敗しました。',
+            'image_url': '画像の取得に失敗しました。',
+            'content': '記事内容の取得に失敗しました。'
+        })
+    return news_list
+
+
+# ----------ニュースサイト要約(検索用)----------#
+def search_news(target, sort, count, limit):
+
+    # ニュースを格納する空のリスト
+    news_list = []
+
+    # ニュースapiのurl
+    url = 'https://newsapi.org/v2/everything'
+
+    # 環境変数からAPIKEYを取得
+    load_dotenv()
+
+    NEWS_API_KEY = os.environ["NEWS_API_KEY"]
+
+    # ヘッダーの設置
+    headers = {'X-Api-Key': NEWS_API_KEY}
+
+    params = {   
+        'q': target,
+        'sortBy': sort,
+        'pageSize': count
+    }
+
+    # リクエストを送信してレスポンスを取得
+    response = requests.get(url, headers=headers, params=params)
+
+    # 記事が取得できたか否か
+    if response.status_code == 200:
+        # JSON形式のデータを取得
+        news_data = response.json()
+        # 記事を取得
+        articles = news_data['articles']
+
+        # 記事を表示
+        for article in articles:
+            # タイトル
+            title = article['title']
+            print(title)
+            # URL
+            url = article['url'] 
+            print(url)
+            # 画像のURL
+            image_url = article['urlToImage'] 
+            print(image_url)
+            # 記事の内容
+            content = article['description'] 
+
+            if content is not None:
+                # 文字列に変換（もし必要なら）
+                if not isinstance(content, str):
+                    content = str(content)
+
+            else:
+                content = '記事の取得に失敗しました。'
+
+            # リストに追加
+        news_list.append({
+            'title': title,
+            'url': url,
+            'image_url': image_url,
+            'content': content
+        })
+    else:
+        news_list.append({
+            'title': 'タイトルの取得に失敗しました。',
+            'url': 'URLの取得に失敗しました。',
+            'image_url': '画像の取得に失敗しました。',
+            'content': '記事内容の取得に失敗しました。'
+        })
+    return news_list
